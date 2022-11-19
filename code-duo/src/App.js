5aa26e3dac91874d461ce76e997612d8bd120f74
Azazel-Woodwind <azazelwoodwind11@gmail.com>
2022-11-19 18:02:36 +0000

    reformat

--- code-duo/src/App.js
+++ code-duo/src/App.js
@@ -1,30 +1,27 @@
-import './App.css';
+import "./App.css";
 // import Profile from './components/Profile'
-import LoginButton from './components/LoginButton';
-import LogoutButton from './components/LogoutButton';
-import Profile from './components/Profile';
+import LoginButton from "./components/LoginButton";
+import LogoutButton from "./components/LogoutButton";
+import Profile from "./components/Profile";
 function App() {
-  return (
-    <div className='   '>
-      <head>
-        <title>
-          Name_of_Project
-        </title>
-      </head>
+    return (
+        <div className="   ">
+            <head>
+                <title>Name_of_Project</title>
+            </head>
 
-      <span className='h-12 border border-ruby-400 flex flex-row'>
-        <LoginButton></LoginButton>
-      </span>
+            <span className="h-12 border border-ruby-400 flex flex-row">
+                <LoginButton />
+            </span>
 
-      <Profile></Profile>
-      <span>
-        <LogoutButton></LogoutButton>
-      </span>
+            <Profile />
+            <span>
+                <LogoutButton />
+            </span>
 
-
-      {/* <Profile></Profile> */}
-    </div>
-  );
+            {/* <Profile></Profile> */}
+        </div>
+    );
 }
 
 export default App;
