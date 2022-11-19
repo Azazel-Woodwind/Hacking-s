89e479d692d84d32078ee4a6d3c2e3d71d0a7710
Azazel-Woodwind <azazelwoodwind11@gmail.com>
2022-11-19 18:47:18 +0000

    ok

--- code-duo/src/App.jsx
+++ code-duo/src/App.jsx
@@ -1,22 +1,14 @@
-import './App.css';
-// import Profile from './components/Profile'
-import LoginButton from './components/LoginButton';
-import LogoutButton from './components/LogoutButton';
-import Profile from './components/Profile';
-import LoginBox from './components/LoginBox';
+import "./App.css";
+import LoginBox from "./components/LoginBox";
+
 function App() {
-  return (
-    <div className='w-screen h-screen bg-gradient-to-r from-[#EFB5A2] to-[#EE7AA0]'>
-      {/* <div className=''>
-        
-      </div> */}
-
-      <div className="flex items-center justify-center h-screen">
-        <LoginBox ></LoginBox>
-      </div>
-    </div>
-
-  );
+    return (
+        <div className="w-screen h-screen bg-gradient-to-r from-[#EFB5A2] to-[#EE7AA0]">
+            <div className="flex items-center justify-center h-screen">
+                <LoginBox />
+            </div>
+        </div>
+    );
 }
 
 export default App;
